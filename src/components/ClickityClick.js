c4dd7487e5088ad53fbe134641cddc6299560085
Veronica Bustabad <vbustabad@gmail.com>
2018-10-16 03:26:18 +0000

    automatically backed up by learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -12,8 +12,7 @@ class ClickityClick extends React.Component {
   handleClick = () => {
     this.setState({
       hasBeenClicked: true
-    })
-    console.log(this.state.hasBeenClicked); // prints false
+    }, () => console.log(this.state.hasBeenClicked))
   }
 
   render() {
